313e5681bf28ee7ba72151e32a2e93acf94a525c
FriendlyWizard23 <riemeralessandro@gmail.com>
2021-11-04 12:05:31 +0100

    commit 2
    Added first basic implementation of the Elettore class

--- src/main/java/mainsystem/Elettore.java
+++ src/main/java/mainsystem/Elettore.java
@@ -1,5 +1,69 @@
 package mainsystem;
 
 public class Elettore {
+	private String nome;
+	private String cognome;
+	private String CF;
+	public Elettore() {}
+	public Elettore(String nome, String cognome, String cF) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		CF = cF;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getCF() {
+		return CF;
+	}
+	public void setCF(String cF) {
+		CF = cF;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((CF == null) ? 0 : CF.hashCode());
+		result = prime * result + ((cognome == null) ? 0 : cognome.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Elettore other = (Elettore) obj;
+		if (CF == null) {
+			if (other.CF != null)
+				return false;
+		} else if (!CF.equals(other.CF))
+			return false;
+		if (cognome == null) {
+			if (other.cognome != null)
+				return false;
+		} else if (!cognome.equals(other.cognome))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
 
+	
 }
